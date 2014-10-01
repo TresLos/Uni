75cb9523cc654bd8402c216eb4d2e6561a9e83ca
ARNAU SISTACH REINOSO <asistare7.alumnes@ub052229.mat.ub.edu>
2014-10-01 09:15:28 +0200

    acabada la practica 2

--- Practiques/2Practica/practica.c
+++ Practiques/2Practica/practica.c
@@ -1,16 +1,39 @@
 #include <stdio.h>
 #include <math.h>
 
+#include "magia.h"
+
 int main (void)
 {
-	int i;
-	double xDirecte, xDoubleRecursiu, xDoubleRecursiu1;
-	float xFloat, xFloat1;
-	double errorAbolutFloatRelatiu, errorRelatiuFloatRecursiu, errorAbsolutDdoubleRecursiu, errorRelatiuDoubleRecursiu;
-	for (i = 0; i < 30; i++)
+// Valors que donem per a operar per comenzar
+	int i, n = 30;
+	double x0 = 1, x1 = 1/3.;
+
+	double xDirecte, xDoubleRecursiu = x1, xDoubleRecursiu1 = x0, xDoubleTmp;
+	float xFloat = (float) x1, xFloat1 = (float) x0, xFloatTmp;
+
+	double errorAbsolutDdoubleRecursiu, errorRelatiuDoubleRecursiu;
+	float errorAbolutFloatRelatiu, errorRelatiuFloatRecursiu;
+
+	printf ("   i   Directe               Float               Err Abs               Err Relatiu               Double               Err Abs               Err Relatiu\n");
+
+	for (i = 0; i < n; i++)
 	{
-		
-		printf ("%3d\t%15.10le\t%15.10lf\t%15.10le\t%15.10le\t%15.10le\t%15.10le\t%15.10le\n", i, xDirecte, xFloat, errorAbolutFloatRelatiu, errorRelatiuFloatRecursiu, xDoubleRecursiu, errorAbsolutDdoubleRecursiu, errorRelatiuDoubleRecursiu);
+		xDirecte = DoubleDirecte (n, x0, x1);
+
+		xDoubleTmp		= DoubleRecursiu (xDoubleRecursiu, xDoubleRecursiu1);
+		xDoubleRecursiu1	= xDoubleRecursiu;
+		xDoubleRecursiu		= xDoubleTmp;
+		errorAbsolutDdoubleRecursiu	= xDoubleRecursiu - xDirecte;
+		errorRelatiuDoubleRecursiu	= errorAbsolutDdoubleRecursiu / xDirecte;
+
+		xFloatTmp		= FloatRecursiu (xFloat, xFloat1);
+		xFloat1			= xFloat;
+		xFloat			= xFloatTmp;
+		errorAbolutFloatRelatiu	= xFloat - xDirecte;
+		errorRelatiuFloatRecursiu	= errorAbolutFloatRelatiu / xDirecte;
+
+		printf ("%3d   %15.10le     %15.10le     %15.10le     %15.10le     %15.10le     %15.10le     %15.10le\n", i, xDirecte, xFloat, errorAbolutFloatRelatiu, errorRelatiuFloatRecursiu, xDoubleRecursiu, errorAbsolutDdoubleRecursiu, errorRelatiuDoubleRecursiu);
 	}
 	return 0;
 }
