acd3c0b869c8d5105e69f5752fe939fecdce7151
ARNAU SISTACH REINOSO <asistare7.alumnes@ub052229.mat.ub.edu>
2014-10-01 14:44:47 +0200

    3era practica

--- Practiques/3Practica/1exercici.c
+++ Practiques/3Practica/1exercici.c
@@ -3,8 +3,23 @@
 
 #include "magia.h"
 
+void printeja (double alpha, double c, double x)
+{
+	//double yaprox, yexac;
+	int i;
+	double j = 1/100.;
+	for (i = 1; i < 20; i++)
+		printf ("%10le     %10le\n", c + j*i, BenMalCondicionat (alpha, x, c + j * i) - BenMalCondicionat (alpha, x, c));
+}
+
 int main (void)
 {
+	double alpha = 1, c = 2, x = 10;
+	printf ("\nMostra per -2:\n\n");
+	printeja (alpha, c, x);
+	c = -2;
+	printf ("\nMostra per 2:\n\n");
+	printeja (alpha, c, x);
 
 	return 0;
-}
+}// Per a -2 dona uns resultats que fan por "Mal plantejat"
--- Practiques/3Practica/magia.h
+++ Practiques/3Practica/magia.h
@@ -1,6 +1,6 @@
-double BenMalCondicionat (double alpha, double c) { return (alpha + 1/c) exp (c * alpha) - 1/c; }
+double BenMalCondicionat (double alpha, double x, double c) { return (alpha + 1/c) * exp (c * x) - 1/c; }
 
 
-double FuntionY (double x) { return 1/2. (exp (-2*x) + 1) }
-double DirecteEstableInestable (double x, double h) { return (FuntionY (x + h) - FuntionY (x - h))/ 2*h; }
+double FuntionY (double x) { return 1/2. * (exp (-2*x) + 1); }
+double DirecteEstableInestable (double x, double h) { return (FuntionY (x + h) - FuntionY (x - h))/ 2*h; }// Derivable... no en faig res
 double RecursiveEstableInestable (double yn1, double yn, double h) { return yn1 + 2*h * (-2*yn + 1); }
