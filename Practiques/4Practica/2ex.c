c39ff26b8252ed005c4318ba9875392879d114a2
TresLos <asistare7@gmail.com>
2014-10-06 16:51:46 +0200

    Ja se pot dir que acabadet esta la practica 4

--- Practiques/4Practica/2ex.c
+++ Practiques/4Practica/2ex.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 #include "matriu.h"
 
@@ -7,6 +8,25 @@
 int main (void)
 {
 	// Testeix del programa en si
-	int a = trisup ();
+//	int a = trisup ();
+	printf ("Entri dim de la matriu:\n")
+	scanf ("%d%d", &n)
+	printf ("\nEntri la matriu:\n")
+	double **U = GMM (n, n);
+	printf ("\nEntri la vector:\n")
+	double *b = GVM (n);
+	printf ("\nEl que has entrat mostrat:");
+	switch (trisup (n, U, b, tol))
+	{
+	case 0:
+		SM (b, 2, 2);
+		break;
+	case 1:
+		printf ("diagonal < %le", tol);
+		break;
+	case -1:
+		printf ("No es una matriu triangular superior");
+		break;
+	}
 	return 0;
 }
